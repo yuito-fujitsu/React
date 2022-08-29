13950db95dacd097205ddf7fa1f23eafb97de7d1
yuito-fujitsu <112158186+yuito-fujitsu@users.noreply.github.com>
2022-08-29 16:56:33 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { useCallback } from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return <hi>こんにちは！</hi>;
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
