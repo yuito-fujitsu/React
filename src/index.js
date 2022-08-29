f1a9c2e88348c1909d5b621355a5749e2af9e421
yuito-fujitsu <112158186+yuito-fujitsu@users.noreply.github.com>
2022-08-29 17:23:19 +0900

    a

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React, { useCallback } from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  return <hi>こんにちは！</hi>;
+  return (
+    <>
+      <hi>こんにちは！</hi>
+      <p>お元気ですか</p>
+    </>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
